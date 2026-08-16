f49e5e34f9ef273606e18b5b84c0173f86420a99
tretmax24-ctrl <tretmax24@gmail.com>
2026-08-17 00:47:27 +0300

    Configure MAX Supabase client

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MAX_CONFIG = {
   supabaseUrl: 'https://vvszkvpamlocmnhxouvi.supabase.co',
-  supabaseAnonKey: 'PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE'
+  supabaseAnonKey: 'sb_publishable_mrLfyC5HZWJ0zDyu06VlPw_66B7iYxX'
 };
